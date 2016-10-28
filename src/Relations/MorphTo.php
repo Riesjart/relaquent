3a243beb90615567876d7140d3382846e44e4062
Riesjart <reesyard@gmail.com>
2016-10-28 16:05:59 +0200

    Add methods to MorphTo relation

--- src/Relations/MorphTo.php
+++ src/Relations/MorphTo.php
@@ -7,6 +7,50 @@ use Illuminate\Database\Eloquent\Relations\MorphTo as BaseMorphTo;
 class MorphTo extends BaseMorphTo
 {
     // =======================================================================//
+    //          Getters
+    // =======================================================================//
+
+    /**
+     * @return mixed
+     */
+    public function getForeignValue()
+    {
+        return $this->parent->{$this->foreignKey};
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getMorphTypeValue()
+    {
+        return $this->parent->{$this->morphType};
+    }
+
+
+    // =======================================================================//
+    //          Flags
+    // =======================================================================//
+
+    /**
+     * @return bool
+     */
+    public function isNull()
+    {
+        return is_null($this->getForeignValue()) || is_null($this->getMorphTypeValue());
+    }
+
+
+    /**
+     * @return bool
+     */
+    public function notNull()
+    {
+        return ! $this->isNull();
+    }
+
+
+    // =======================================================================//
     //          Converters
     // =======================================================================//
 
